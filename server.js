2b1834a2bb9bba6e64c389e64b372685159ac596
browniecakese <23031962@myrp.edu.sg>
2026-01-10 22:18:32 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,83 @@
+//include required packages
+const express = require('express');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+const port=3000;
+
+//database config info
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0,
+};
+
+//initialise express app
+const app = express();
+//helps app to read JSON
+app.use(express.json());
+
+//start the server
+app.listen(port,()=> {
+    console.log('Server running on port', port);
+});
+
+//route: get all flavours
+app.get('/allflavours', async(req,res)=> {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.icecream');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message:'Server error for allflavours'});
+    }
+});
+
+
+//route: add flavours
+app.post('/addflavour',async(req,res)=> {
+    try{
+        const { id, flavour_name,flavour_pic} = req.body;
+
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('INSERT INTO defaultdb.icecream (id,flavour_name,flavour_pic) VALUES (?,?,?)', [id, flavour_name,flavour_pic]);
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message:'Server error for addflavour'});
+    }
+});
+
+//route: update flavour
+app.post('/updateflavour/:id',async(req,res)=> {
+    try{
+        const {id} = req.params;
+        const {flavour_name,flavour_pic} = req.body;
+
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('UPDATE defaultdb.icecream SET id=? , flavour_name=?,flavour_pic=? WHERE id=?', [id, flavour_name, flavour_pic]);
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message:'Server error for updateflavour'});
+    }
+});
+
+//route: delete flavour
+app.post('/deleteflavour/:id',async(req,res)=> {
+    try{
+        const { id} = req.params;
+
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('DELETE FROM defaultdb.icecream WHERE id=?');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message:'Server error for deleteflavour'});
+    }
+});
